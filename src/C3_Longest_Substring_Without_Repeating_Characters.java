624de764077f42deac06c4249bccc6edb78e798d
donagoliag <donagoliag@gmail.com>
2026-04-08 14:24:34 +0100

    LEETCODE CHALLENGES

---
+++ src/C3_Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,47 @@
+/*3. Longest Substring Without Repeating Characters
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s, find the length of the longest substring without duplicate characters.
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+*/
+
+/*
+*(On part des petites sous-chaînes aux grandes)*
+
+**a. Pour n = 1 → longueur de chaîne**
+- Retrouver / Compter toutes les sous-chaînes de longueurs n.
+- Pour chaque sous-chaîne de longueur n, trouver :
+  - Compter les répétitions.
+  - Si ∃ en a, on fait rien
+  - Si il n'y en a pas : max = n, la sous-chaîne est la solution
+  - On peut continuer.
+
+Si max = n (donc on a trouvé) on continue avec la boucle.
+Si max < n, on sort, plus besoin de continuer.*/
+
+public class C3_Longest_Substring_Without_Repeating_Characters {
+}
